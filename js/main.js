125e7778af753d9d5b9e4ec4172642dabca492db
Cesar <cesarsuarez2303@gmail.com>
2025-03-14 22:27:06 +0100

    Added new animations and styles

--- js/main.js
+++ js/main.js
@@ -1,18 +1,25 @@
 // Nav Bar Toggle
 
 let toggleBtn = document.getElementById("toggleBtn");
+let headerTitle = document.querySelector(".nav-title");
+let body = document.getElementById("body");
+
 toggleBtn.addEventListener("click", toggleMenu);
 
 function toggleMenu() {
 	this.classList.toggle("toggled");
 
-	let links = document.getElementById("sideBar");
+	let links = document.getElementById("side-bar");
 	if (
 		links.style.transform === "translateX(-200%)" ||
 		links.style.transform === ""
 	) {
+		headerTitle.style.color = "#c8c5ba";
 		links.style.transform = "translateX(0px)";
+		body.style.overflowY = "hidden";
 	} else {
+		headerTitle.style.color = "#665b55";
 		links.style.transform = "translateX(-200%)";
+		body.style.overflowY = "scroll";
 	}
 }
